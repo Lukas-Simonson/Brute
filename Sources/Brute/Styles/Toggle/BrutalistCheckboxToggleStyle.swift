f88e4569e177de4c71336d5b9de56cdc71ca9db6
Lukas Simonson <lukas@hydradesignllc.com>
2025-11-11 23:56:42 -0700

    Adds BrutalistCheckboxToggleStyle

---
+++ Sources/Brute/Styles/Toggle/BrutalistCheckboxToggleStyle.swift
@@ -0,0 +1,75 @@
+//
+//  BrutalistCheckboxToggleStyle.swift
+//  Brute
+//
+//  Created by Lukas Simonson on 11/11/25.
+//
+
+import SwiftUI
+
+extension ToggleStyle where Self == BrutalistCheckboxToggleStyle {
+    public static var brutalistCheckbox: Self { BrutalistCheckboxToggleStyle() }
+}
+
+public struct BrutalistCheckboxToggleStyle: ToggleStyle {
+
+    @Environment(\.bruteTheme) private var theme
+
+    public func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            Rectangle()
+                .fill(
+                    configuration.isOn ? theme.color.secondaryBackground : theme.color.tertiaryBackground
+                )
+                .stroke(theme.color.border, lineWidth: theme.dimen.borderWidth)
+                .aspectRatio(1, contentMode: .fit)
+                .frame(maxHeight: 25)
+                .overlay {
+                    if configuration.isOn {
+                        Image(systemName: "checkmark")
+                            .foregroundStyle(theme.color.secondaryForeground)
+                    }
+                }
+
+            configuration.label
+        }
+        .onTapGesture {
+            configuration.$isOn.wrappedValue = !configuration.isOn
+        }
+    }
+}
+
+#Preview {
+
+    @Previewable @Environment(\.bruteTheme) var theme
+    @Previewable @State var isOn = true
+
+    VStack(alignment: .leading) {
+        Toggle(isOn: $isOn) {
+            Text("Violet Checkbox")
+        }
+        .toggleStyle(.brutalistCheckbox)
+        .environment(\.bruteTheme, .violet)
+
+        Toggle(isOn: $isOn) {
+            Text("Blue Checkbox")
+        }
+        .toggleStyle(.brutalistCheckbox)
+        .environment(\.bruteTheme, .blue)
+
+        Toggle(isOn: $isOn) {
+            Text("Orange Checkbox")
+        }
+        .toggleStyle(.brutalistCheckbox)
+        .environment(\.bruteTheme, .orange)
+
+        Toggle(isOn: $isOn) {
+            Text("Green Checkbox")
+        }
+        .toggleStyle(.brutalistCheckbox)
+        .environment(\.bruteTheme, .green)
+    }
+    .padding()
+    .background(theme.color.background)
+    .brutalized()
+}
