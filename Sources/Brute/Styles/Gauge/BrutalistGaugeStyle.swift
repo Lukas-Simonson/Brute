47e23ee1a03fd0e0a85873351268082c078eb252
Lukas Simonson <lukas@hydradesignllc.com>
2025-11-13 21:13:04 -0700

    Migrates BrutalistGaugeStyle

--- Sources/Brute/Styles/Gauge/BrutalistGaugeStyle.swift
+++ Sources/Brute/Styles/Gauge/BrutalistGaugeStyle.swift
@@ -13,7 +13,7 @@ extension GaugeStyle where Self == BrutalistGaugeStyle {
 
 public struct BrutalistGaugeStyle: GaugeStyle {
 
-    @Environment(\.bruteTheme) private var theme
+    @Environment(\.bruteContext) private var context
 
     public func makeBody(configuration: Configuration) -> some View {
         VStack {
@@ -25,7 +25,7 @@ public struct BrutalistGaugeStyle: GaugeStyle {
             if let label = configuration.currentValueLabel {
                 label
                     .frame(maxWidth: .infinity, alignment: .leading)
-                    .padding(theme.dimen.smallContentPadding)
+                    .padding(context.dimen.paddingSmall)
                     .background(progressView(value: configuration.value))
             } else {
                 progressView(value: configuration.value)
@@ -41,10 +41,10 @@ public struct BrutalistGaugeStyle: GaugeStyle {
     }
 
     private func progressView(value: Double) -> some View {
-        PercentFillShape(percent: value, cornerRadius: theme.dimen.cornerRadius)
-            .fill(theme.color.secondaryBackground)
-            .stroke(theme.color.border, lineWidth: theme.dimen.borderWidth)
-            .background(theme.color.tertiaryBackground)
+        PercentFillShape(percent: value, cornerRadius: context.dimen.cornerRadius)
+            .fill(context.color.accentBackground)
+            .stroke(context.color.border, lineWidth: context.dimen.borderWidth)
+            .background(context.color.background)
             .bruteClipped()
             .bruteStroked()
     }
@@ -54,56 +54,63 @@ public struct BrutalistGaugeStyle: GaugeStyle {
 
     @Previewable @Environment(\.bruteTheme) var theme
 
-    VStack {
-        Gauge(value: 0.5) {
-            Text("Violet")
-        }
-        .gaugeStyle(.brute)
-        .bruteTheme(.violet)
-
-        Gauge(
-            value: 0.09,
-            label: {
-                Text("Blue")
-            },
-            currentValueLabel: {
-                Text("15%")
-            },
-            minimumValueLabel: {
-                Text("0%")
-            },
-            maximumValueLabel: {
-                Text("100%")
+    BruteStyle {
+        VStack {
+            Gauge(value: 0.5) {
+                Text("Violet")
             }
-        )
-        .gaugeStyle(.brute)
-        .bruteTheme(.blue)
+            .gaugeStyle(.brute)
+            .withLeveledBruteTheme(.violet)
+            // .bruteTheme(.violet)
 
-        Gauge(value: 0.5) {
-            Text("Orange")
-        }
-        .gaugeStyle(.brute)
-        .bruteTheme(.orange)
+            Gauge(
+                value: 0.09,
+                label: {
+                    Text("Blue")
+                },
+                currentValueLabel: {
+                    Text("15%")
+                },
+                minimumValueLabel: {
+                    Text("0%")
+                },
+                maximumValueLabel: {
+                    Text("100%")
+                }
+            )
+            .gaugeStyle(.brute)
+            .withLeveledBruteTheme(.blue)
+            // .bruteTheme(.blue)
 
-        Gauge(
-            value: 0.75,
-            label: {
-                Text("Green")
-            },
-            currentValueLabel: {
-                Text("75%")
-            },
-            minimumValueLabel: {
-                Text("0%")
-            },
-            maximumValueLabel: {
-                Text("100%")
+            Gauge(value: 0.5) {
+                Text("Orange")
             }
-        )
-        .gaugeStyle(.brute)
-        .bruteTheme(.green)
+            .gaugeStyle(.brute)
+            .withLeveledBruteTheme(.orange)
+
+            Gauge(
+                value: 0.75,
+                label: {
+                    Text("Green")
+                },
+                currentValueLabel: {
+                    Text("75%")
+                },
+                minimumValueLabel: {
+                    Text("0%")
+                },
+                maximumValueLabel: {
+                    Text("100%")
+                }
+            )
+            .gaugeStyle(.brute)
+            .withLeveledBruteTheme(.green)
+
+            Gauge(value: 0.66) {
+                Text("Multi")
+            }
+            .withLeveledBruteTheme(.multi)
+        }
+        .padding()
     }
-    .padding()
-    .frame(maxWidth: .infinity, maxHeight: .infinity)
-    .background(theme.color.background)
 }
